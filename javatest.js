15a1e970311bfe0003c03121cd99f37c35ecd209
dharikamal <76258348+dharikamal@users.noreply.github.com>
2021-02-03 19:22:23 +0300

    Create javatest.js

---
+++ javatest.js
@@ -0,0 +1,42 @@
+
+
+//for (let i = 0 ; i<10 ;i++){
+  //  console.log(i)
+//}
+
+//AD();
+
+//function AD(){
+   // console.log("hi")
+//}
+
+//function addnum(x){
+    //console.log(x+1)
+//}
+
+//function addnum(x,y){
+  //  console.log(x+y)
+//}
+//addnum(9,10)
+
+//function add(x,y){
+  //  console.log(x+y)
+//}
+//const firstnum = 10
+//const scndnum = 12
+//const result = add(firstnum, scndnum);
+//console.log(result);
+
+//function addno(x){
+  //  return x+1;
+//}
+
+//const result = addno(7);
+//console.log(result)
+
+function add(a,b){
+    const result = a+b;
+    return result;
+}
+const result = add(7,4)
+console.log(result)
